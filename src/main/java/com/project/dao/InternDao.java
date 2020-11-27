9a084201102ed951bc534bb1ec72f28f1818d5af
Thomas <thomas.guillaume@efrei.net>
2020-11-27 15:12:03 +0100

    fix :hammer:

--- src/main/java/com/project/dao/InternDao.java
+++ src/main/java/com/project/dao/InternDao.java
@@ -2,10 +2,7 @@ package com.project.dao;
 
 
 import com.project.database.Database;
-import com.project.exceptions.DaoException;
 import com.project.models.Intern;
-
-import java.util.List;
 
 /**
  * Service class to handle Intern table
@@ -13,93 +10,7 @@ import java.util.List;
 public class InternDao extends DaoResource<Intern> {
 
     public InternDao(Database database) {
-        this.database = database;
-        this.entityManager = this.database.getConnection();
+        super(database, Intern.class);
     }
-
-    @Override
-    public List<Intern> findAll() throws DaoException {
-        isOpen();
-        try {
-            return entityManager.createQuery("SELECT c FROM Intern c", Intern.class).getResultList();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public Intern find(int id) throws DaoException {
-        isOpen();
-        try {
-            return entityManager.find(Intern.class, id);
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void save(Intern object) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(object);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-
-    public void saveAll(Intern... objects) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            for (Intern object : objects) {
-                entityManager.persist(object);
-            }
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void update(Intern object) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.merge(object);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void updateAll(List<Intern> objects) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            objects.forEach(entityManager::merge);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-
-
 
 }
--- src/main/java/com/project/dao/SupervisorDao.java
+++ src/main/java/com/project/dao/SupervisorDao.java
@@ -1,96 +1,11 @@
 package com.project.dao;
 
 import com.project.database.Database;
-import com.project.exceptions.DaoException;
 import com.project.models.Supervisor;
-
-import java.util.List;
 
 public class SupervisorDao extends DaoResource<Supervisor> {
 
     public SupervisorDao(Database database) {
-        this.database = database;
-        this.entityManager = this.database.getConnection();
-    }
-
-    @Override
-    public List<Supervisor> findAll() throws DaoException {
-        isOpen();
-        try {
-            return entityManager.createQuery("SELECT c FROM Supervisor c", Supervisor.class).getResultList();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public Supervisor find(int id) throws DaoException {
-        isOpen();
-        try {
-            return entityManager.find(Supervisor.class, id);
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void save(Supervisor object) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.persist(object);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    public void saveAll(Supervisor... objects) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            for (Supervisor object : objects) {
-                entityManager.persist(object);
-            }
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void update(Supervisor object) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            entityManager.merge(object);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
-    }
-
-    @Override
-    public void updateAll(List<Supervisor> objects) throws DaoException {
-        isOpen();
-        try {
-            entityManager.getTransaction().begin();
-            objects.forEach(entityManager::merge);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            throw new DaoException(e);
-        } finally {
-            entityManager.close();
-        }
+        super(database, Supervisor.class);
     }
 }
